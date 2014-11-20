74cdc3261fb455063a9498e0b808679a4bb16d1c
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-11-20 19:00:45 +0100

    bug in bench for imm js

--- dist/immutato.js
+++ dist/immutato.js
@@ -1,5 +1,4 @@
 (function (name, factory) {
-    
     if (typeof define === 'function' && define.amd) {
         define([], factory);
     } else if (typeof exports === 'object') {
@@ -24,83 +23,109 @@
     _require.modules = [function (module, exports) {
             'use strict';
             var I = function () {
-                    function buildFirstPropertyIndexes(keysLength) {
+                    var cacheCount = 300;
+                    var caches = [];
+                    function buildPropertyIndexes(keysLength) {
                         var propertyIndexes = {};
                         var i = keysLength;
                         while (i--) {
                             propertyIndexes[i] = 0;
                         }
                         propertyIndexes.indexGeneration = 0;
-                        Object.freeze(propertyIndexes);
                         return propertyIndexes;
                     }
-                    function get(propertyIndexes, opts) {
-                        var propertyIndex = opts.propertyIndex;
-                        var propertyName = opts.propertyName;
-                        var transactions = opts.transactions;
-                        var transactionIndex = propertyIndexes[propertyIndex];
-                        var transaction = transactions[transactionIndex];
-                        return transaction[propertyName];
+                    var k = cacheCount;
+                    while (k--) {
+                        caches[k] = buildPropertyIndexes(k);
                     }
-                    function set(value, propertyIndexes, opts) {
+                    function buildFirstPropertyIndexes(keysLength) {
+                        if (keysLength > cacheCount) {
+                            console.log('CACHE REBUILD');
+                            return buildPropertyIndexes(keysLength);
+                        } else {
+                            return caches[keysLength];
+                        }
+                    }
+                    function get(self, opts) {
+                        var propertyIndexes = self.propertyIndexes;
+                        var instanceId = self.instanceId;
                         var propertyIndex = opts.propertyIndex;
-                        var propertyName = opts.propertyName;
-                        var transactions = opts.transactions;
-                        var keysLength = opts.keysLength;
+                        var transactionsForClass = opts.transactionsForClass;
+                        var transactionIndex = propertyIndexes[propertyIndex];
+                        var transactions = transactionsForClass[instanceId];
+                        var transaction = transactions[transactionIndex];
+                        return transaction[propertyIndex];
+                    }
+                    var create = function () {
+                            function C() {
+                            }
+                            return function create(proto, properties) {
+                                C.prototype = proto;
+                                var instance = new C();
+                                return Object.defineProperties(instance, properties);
+                            };
+                        }();
+                    function set(value, self, opts) {
+                        var propertyIndexes = self.propertyIndexes;
+                        var instanceId = self.instanceId;
+                        var propertyIndex = opts.propertyIndex;
+                        var transactionsForClass = opts.transactionsForClass;
                         var Contructor = opts.Contructor;
-                        var newTransaction = {};
-                        newTransaction[propertyName] = value;
+                        var transactions = transactionsForClass[instanceId];
+                        var newTransaction = new Array(propertyIndex + 1);
+                        newTransaction[propertyIndex] = value;
                         transactions.push(newTransaction);
                         var newPropertyIndexes;
-                        if (propertyIndexes.indexGeneration === 100) {
-                            newPropertyIndexes = {};
-                            var i = keysLength;
-                            while (i--) {
-                                newPropertyIndexes[i] = propertyIndexes[i];
-                            }
-                            newPropertyIndexes[propertyIndex] = transactions.length - 1;
-                            newPropertyIndexes.indexGeneration = 0;
-                        } else {
-                            var propertyIndexDescriptor = { indexGeneration: { value: propertyIndexes.indexGeneration + 1 } };
-                            propertyIndexDescriptor[propertyIndex] = { value: transactions.length - 1 };
-                            newPropertyIndexes = Object.create(propertyIndexes, propertyIndexDescriptor);
-                        }
-                        Object.freeze(newPropertyIndexes);
-                        return new Contructor(newPropertyIndexes);
+                        var propertyIndexDescriptor = {};
+                        propertyIndexDescriptor[propertyIndex] = { value: transactions.length - 1 };
+                        newPropertyIndexes = create(propertyIndexes, propertyIndexDescriptor);
+                        return new Contructor(newPropertyIndexes, instanceId);
                     }
                     function mkGetterSetter(opts) {
                         return function getterSetter(value) {
                             if (typeof value === 'undefined') {
-                                return get(this.propertyIndexes, opts);
+                                return get(this, opts);
                             } else {
-                                return set(value, this.propertyIndexes, opts);
+                                return set(value, this, opts);
                             }
                         };
                     }
-                    function I(data) {
-                        var keys = Object.keys(data);
+                    function I(dataSample) {
+                        var keys = Object.keys(dataSample);
                         var keysLength = keys.length;
-                        var transactions = [data];
-                        var propertyIndexes = buildFirstPropertyIndexes(keysLength);
+                        var transactionsForClass = [];
                         var Proto = {};
-                        function Contructor(propertyIndexes) {
-                            this.propertyIndexes = propertyIndexes;
-                            Object.freeze(this);
-                        }
-                        Contructor.prototype = Proto;
                         var i = keysLength;
+                        var clonedData = {};
                         while (i--) {
                             var opts = {
                                     propertyIndex: i,
-                                    propertyName: keys[i],
-                                    transactions: transactions,
+                                    transactionsForClass: transactionsForClass,
                                     Contructor: Contructor,
                                     keysLength: keysLength
                                 };
-                            Proto[opts.propertyName] = mkGetterSetter(opts);
+                            var propertyName = keys[i];
+                            Proto[propertyName] = mkGetterSetter(opts);
                         }
-                        Object.freeze(Proto);
-                        return new Contructor(propertyIndexes);
+                        function Contructor(propertyIndexes, instanceId) {
+                            this.propertyIndexes = propertyIndexes;
+                            this.instanceId = instanceId;
+                        }
+                        Contructor.prototype = Proto;
+                        var instanceId = 0;
+                        return function (data) {
+                            var propertyIndexes = buildFirstPropertyIndexes(keysLength);
+                            var clonedData = [];
+                            i = 0;
+                            while (i < keysLength) {
+                                clonedData.push(data[keys[i]]);
+                                i++;
+                            }
+                            transactionsForClass[instanceId] = [clonedData];
+                            var res = new Contructor(propertyIndexes, instanceId);
+                            instanceId++;
+                            return res;
+                        };
                     }
                     return I;
                 }();
